8fe7c1bd0dcb2f22beb7ab164723fe073c4404b8
keu santos <modulodigital69@gmail.com>
2022-02-05 19:40:11 -0300

    Aperfeiçoando com JQuery

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    function pegarIdioma() {
+        var idiomas = []
+        $.ajax({
+            method:"get",
+            url:"https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+            headers:{
+                "accept-encoding":"application/gzip",
+                "x-rapidapi-host":"google-translate1.p.rapidapi.com",
+                "x-rapidapi-key":"5d6c0ce980msh491bd586a6f34fcp1561adjsnd2493b085289"
+            } 
+        }).done(function(data){
+           idiomas = data.data.languages
+           for (var x=0; x<idiomas.length;x++) {
+               $("#idioma").append(
+                `<option value="${idiomas[x].language}">${idiomas[x].language}</option>`
+               )
+               
+           }
+        })
+    } 
+    $("#texto-original").keyup(function(){
+       var textoOriginal =  $("#texto-original").val()
+       var idioma = $("#idioma").val() 
+       $.ajax({
+        "url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "accept-encoding": "application/gzip",
+            "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+            "x-rapidapi-key":"5d6c0ce980msh491bd586a6f34fcp1561adjsnd2493b085289"
+        },
+        "data": {
+            "source": idioma,
+            "target": "pt",
+            "q": textoOriginal
+        }
+       }).done(function(data){
+           $("#traducao").text(data.data.translations[0].translatedText)
+       })
+    })
+    pegarIdioma()
+})
